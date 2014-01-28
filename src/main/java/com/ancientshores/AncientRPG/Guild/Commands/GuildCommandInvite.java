0a35114372c48d3dc102538cf7a1c338cf1404dd
xXKeyleXx <keyle@t-online.de>
2014-01-28 12:37:24 +0100

    check player for null before using the player fixes #3

--- src/main/java/com/ancientshores/AncientRPG/Guild/Commands/GuildCommandInvite.java
+++ src/main/java/com/ancientshores/AncientRPG/Guild/Commands/GuildCommandInvite.java
@@ -9,56 +9,36 @@ import org.bukkit.entity.Player;
 
 public class GuildCommandInvite {
     public static void processInvite(CommandSender sender, String[] args) {
-        Player mPlayer = (Player) sender;
+        Player player = (Player) sender;
         if (args.length == 2) {
-            AncientRPGGuild mGuild = AncientRPGGuild.getPlayersGuild(mPlayer
-                    .getName());
-            if (mGuild != null) {
-                if (AncientRPGGuildRanks.hasInviteRights(mGuild.gMember
-                        .get(mPlayer.getName()))) {
-                    Player mInvitedPlayer = AncientRPG.plugin.getServer()
-                            .getPlayer(args[1]);
-                    if (AncientRPGGuild.getPlayersGuild(mInvitedPlayer
-                            .getName()) == null
-                            && mInvitedPlayer != null
-                            && !AncientRPGGuild.invites
-                            .containsKey(mInvitedPlayer)) {
-                        if (AncientRPG.hasPermissions(mInvitedPlayer,
-                                AncientRPGGuild.gNodeJoin)) {
+            AncientRPGGuild guild = AncientRPGGuild.getPlayersGuild(player.getName());
+            if (guild != null) {
+                if (AncientRPGGuildRanks.hasInviteRights(guild.gMember.get(player.getName()))) {
+                    Player invitedPlayer = AncientRPG.plugin.getServer().getPlayer(args[1]);
+                    if (invitedPlayer != null && AncientRPGGuild.getPlayersGuild(invitedPlayer.getName()) == null && !AncientRPGGuild.invites.containsKey(invitedPlayer)) {
+                        if (AncientRPG.hasPermissions(invitedPlayer, AncientRPGGuild.gNodeJoin)) {
                             if (AncientRPGGuild.invites.size() < AncientRPGGuild.maxPlayers) {
-                                AncientRPGGuild.invites.put(mInvitedPlayer,
-                                        mGuild);
-                                mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.GREEN
-                                        + "Invited " + mInvitedPlayer.getName()
-                                        + " to your guild.");
-                                mInvitedPlayer.sendMessage(AncientRPG.brand2 + ChatColor.GREEN
-                                        + "You were invited to the guild "
-                                        + "\"" + mGuild.guildName + "\"" + " by "
-                                        + mPlayer.getName() + ".");
-                                mInvitedPlayer.sendMessage(AncientRPG.brand2 + ChatColor.GREEN
-                                        + "Use /guild accept or/guild reject.");
+                                AncientRPGGuild.invites.put(invitedPlayer, guild);
+                                player.sendMessage(AncientRPG.brand2 + ChatColor.GREEN + "Invited " + invitedPlayer.getName() + " to your guild.");
+                                invitedPlayer.sendMessage(AncientRPG.brand2 + ChatColor.GREEN + "You were invited to the guild " + "\"" + guild.guildName + "\"" + " by " + player.getName() + ".");
+                                invitedPlayer.sendMessage(AncientRPG.brand2 + ChatColor.GREEN + "Use /guild accept or/guild reject.");
                             } else {
-                                mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.RED
-                                        + "Your guild is already full.");
+                                player.sendMessage(AncientRPG.brand2 + ChatColor.RED + "Your guild is already full.");
                             }
                         } else {
-                            mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.RED
-                                    + "This Player hasn't the permission to join a guild.");
+                            player.sendMessage(AncientRPG.brand2 + ChatColor.RED + "This Player hasn't the permission to join a guild.");
                         }
                     } else {
-                        mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.RED
-                                + "This Player already has a guild or doesn't exist or already is invited.");
+                        player.sendMessage(AncientRPG.brand2 + ChatColor.RED + "This Player already has a guild or doesn't exist or already is invited.");
                     }
                 } else {
-                    mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.RED
-                            + "You don't have the rank to invite people.");
+                    player.sendMessage(AncientRPG.brand2 + ChatColor.RED + "You don't have the rank to invite people.");
                 }
             } else {
-                mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.RED + "You are in no guild.");
+                player.sendMessage(AncientRPG.brand2 + ChatColor.RED + "You are in no guild.");
             }
         } else {
-            mPlayer.sendMessage(AncientRPG.brand2 + ChatColor.RED
-                    + "Correct usage: /guild invite <player>");
+            player.sendMessage(AncientRPG.brand2 + ChatColor.RED + "Correct usage: /guild invite <player>");
         }
     }
 }
