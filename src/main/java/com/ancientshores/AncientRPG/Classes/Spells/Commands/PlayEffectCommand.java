af9823783bb3181eb781ed24006c0743ffcf16b0
FroznMine <pesafrre@arcor.de>
2014-11-19 21:36:04 +0100

    Fixed effect bug
    The bug that made it impossible to use PlayEffect and PlayEntityEffect
    in spells is now fixed
    
    fixes #50

--- src/main/java/com/ancientshores/AncientRPG/Classes/Spells/Commands/PlayEffectCommand.java
+++ src/main/java/com/ancientshores/AncientRPG/Classes/Spells/Commands/PlayEffectCommand.java
@@ -7,42 +7,39 @@ import com.ancientshores.AncientRPG.Classes.Spells.CommandDescription;
 import com.ancientshores.AncientRPG.Classes.Spells.ParameterType;
 
 public class PlayEffectCommand extends ICommand {
-    @CommandDescription(description = "<html>Play effect at the location</html>",
-            argnames = {"location", "effectname"}, name = "PlayEffect", parameters = {ParameterType.Location, ParameterType.String})
-    public PlayEffectCommand() {
-        this.paramTypes = new ParameterType[]{ParameterType.Location, ParameterType.String};
-    }
+	@CommandDescription(description = "<html>Play effect at the location</html>",
+			argnames = {"location", "effectname"}, name = "PlayEffect", parameters = {ParameterType.Location, ParameterType.String})
+	public PlayEffectCommand() {
+		this.paramTypes = new ParameterType[]{ParameterType.Location, ParameterType.String};
+	}
 
-    @Override
-    public boolean playCommand(final EffectArgs ca) {
-        if (ca.getParams().size() == 2 && ca.getParams().get(1) instanceof String && ca.getParams().get(0) instanceof Location[]) {
-            final Location[] loc = (Location[]) ca.getParams().get(0);
-            final String name = (String) ca.getParams().get(1);
-            Effect effect = getParticleEffectByName(name);
-            if (effect == null) {
-                return false;
-            }
-            if (loc != null && loc.length > 0) {
-                for (Location l : loc) {
-                    if (l == null) {
-                        continue;
-                    }
-                    l.getWorld().playEffect(l, effect, 0);
-                }
-                return true;
-            }
-        }
-        return false;
-    }
+	@Override
+	public boolean playCommand(final EffectArgs ca) {
+		if (ca.getParams().size() == 2 && ca.getParams().get(1) instanceof String && ca.getParams().get(0) instanceof Location[]) {
+			final Location[] loc = (Location[]) ca.getParams().get(0);
+			final String name = (String) ca.getParams().get(1);
+			Effect effect = getParticleEffectByName(name);
+			if (effect == null) {
+				return false;
+			}
+			if (loc != null && loc.length > 0) {
+				for (Location l : loc) {
+					if (l == null) {
+						continue;
+					}
+					l.getWorld().playEffect(l, effect, 0);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public static Effect getParticleEffectByName(String s) {
-        s.replace("_", "");
-        for (Effect e : Effect.values()) {
-            String ename = e.name().replace("_", "");
-            if (ename.equalsIgnoreCase(s)) {
-                return e;
-            }
-        }
-        return null;
-    }
+	private static Effect getParticleEffectByName(String s) {
+		s = s.replace("_", "").toUpperCase();
+		for (Effect e : Effect.values())
+			if (e.name().replace("_", "").equalsIgnoreCase(s))
+				return e;
+		return null;
+	}
 }
--- src/main/java/com/ancientshores/AncientRPG/Classes/Spells/Commands/PlayEntityEffect.java
+++ src/main/java/com/ancientshores/AncientRPG/Classes/Spells/Commands/PlayEntityEffect.java
@@ -7,42 +7,39 @@ import com.ancientshores.AncientRPG.Classes.Spells.CommandDescription;
 import com.ancientshores.AncientRPG.Classes.Spells.ParameterType;
 
 public class PlayEntityEffect extends ICommand {
-    @CommandDescription(description = "<html>Play entity effect at the location</html>",
-            argnames = {"entity", "effectname"}, name = "PlayEntityEffect", parameters = {ParameterType.Entity, ParameterType.String})
-    public PlayEntityEffect() {
-        this.paramTypes = new ParameterType[]{ParameterType.Entity, ParameterType.String};
-    }
+	@CommandDescription(description = "<html>Play entity effect at the location</html>",
+			argnames = {"entity", "effectname"}, name = "PlayEntityEffect", parameters = {ParameterType.Entity, ParameterType.String})
+	public PlayEntityEffect() {
+		this.paramTypes = new ParameterType[]{ParameterType.Entity, ParameterType.String};
+	}
 
-    @Override
-    public boolean playCommand(final EffectArgs ca) {
-        if (ca.getParams().size() == 2 && ca.getParams().get(1) instanceof String && ca.getParams().get(0) instanceof Entity[]) {
-            final Entity[] e = (Entity[]) ca.getParams().get(0);
-            final String name = (String) ca.getParams().get(1);
-            EntityEffect effect = getEntityEffectByName(name);
-            if (effect == null) {
-                return false;
-            }
-            if (e != null && e.length > 0) {
-                for (Entity ent : e) {
-                    if (ent == null) {
-                        continue;
-                    }
-                    ent.playEffect(effect);
-                }
-                return true;
-            }
-        }
-        return false;
-    }
+	@Override
+	public boolean playCommand(final EffectArgs ca) {
+		if (ca.getParams().size() == 2 && ca.getParams().get(1) instanceof String && ca.getParams().get(0) instanceof Entity[]) {
+			final Entity[] e = (Entity[]) ca.getParams().get(0);
+			final String name = (String) ca.getParams().get(1);
+			EntityEffect effect = getEntityEffectByName(name);
+			if (effect == null) {
+				return false;
+			}
+			if (e != null && e.length > 0) {
+				for (Entity ent : e) {
+					if (ent == null) {
+						continue;
+					}
+					ent.playEffect(effect);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public static EntityEffect getEntityEffectByName(String s) {
-        s.replace("_", "");
-        for (EntityEffect e : EntityEffect.values()) {
-            String ename = e.name().replace("_", "");
-            if (ename.equalsIgnoreCase(s)) {
-                return e;
-            }
-        }
-        return null;
-    }
+	public static EntityEffect getEntityEffectByName(String s) {
+		s = s.replace("_", "").toUpperCase();
+		for (EntityEffect e : EntityEffect.values())
+			if (e.name().replace("_", "").equalsIgnoreCase(s))
+				return e;
+		return null;
+	}
 }
