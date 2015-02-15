0ecb563bd344d2241cae306b824bb07b398b092c
FroznMine <pesafrre@arcor.de>
2015-02-15 14:59:55 +0100

    Fixed 'Damage' spell command
    The damage spell command didn’t worked because of the switch to UUID
    system. Through rolling that back and updating the code it now should
    work again.

--- src/main/java/com/ancientshores/Ancient/Classes/Spells/Commands/DamageCommand.java
+++ src/main/java/com/ancientshores/Ancient/Classes/Spells/Commands/DamageCommand.java
@@ -1,59 +1,58 @@
 package com.ancientshores.Ancient.Classes.Spells.Commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.ancientshores.Ancient.Ancient;
 import com.ancientshores.Ancient.Classes.Spells.CommandDescription;
 import com.ancientshores.Ancient.Classes.Spells.ParameterType;
+import com.ancientshores.Ancient.HP.CreatureHp;
 import com.ancientshores.Ancient.Listeners.AncientEntityListener;
+import com.ancientshores.Ancient.Listeners.AncientPlayerListener;
 
 public class DamageCommand extends ICommand {
 
-    @CommandDescription(description = "<html>Damages the targeted entity with the specified damage</html>",
-            argnames = {"entity", "damage"}, name = "Damage", parameters = {ParameterType.Entity, ParameterType.Number})
-    public DamageCommand() {
-        this.paramTypes = new ParameterType[]{ParameterType.Entity, ParameterType.Number};
-    }
+	@CommandDescription(description = "<html>Damages the targeted entity with the specified damage</html>",
+			argnames = {"entity", "damage"}, name = "Damage", parameters = {ParameterType.Entity, ParameterType.Number})
+	public DamageCommand() {
+		this.paramTypes = new ParameterType[]{ParameterType.Entity, ParameterType.Number};
+	}
 
-    @Override
-    public boolean playCommand(final EffectArgs ca) {
-        try {
-            if (ca.getParams().get(0) instanceof Entity[] && ca.getParams().get(1) instanceof Number) {
-                final Entity[] target = (Entity[]) ca.getParams().get(0);
-                double damage = ((Number) ca.getParams().get(1)).floatValue();
-                if (target != null && target.length > 0 && target[0] instanceof Entity) {
-                    for (final Entity targetPlayer : target) {
-                        if (targetPlayer == null || !(targetPlayer instanceof LivingEntity)) {
-                            continue;
-                        }
-                        EntityDamageByEntityEvent event = new EntityDamageByEntityEvent(ca.getCaster(), targetPlayer, DamageCause.CUSTOM, damage); 
+	@Override
+	public boolean playCommand(final EffectArgs ca) {
+		try {
+			if (ca.getParams().get(0) instanceof Entity[] && ca.getParams().get(1) instanceof Number) {
+				final Entity[] targets = (Entity[]) ca.getParams().get(0);
+				double damage = ((Number) ca.getParams().get(1)).floatValue();
+				if (targets != null && targets.length > 0 && targets[0] instanceof Entity) {
+					
+					for (final Entity target : targets) {
+						if (target == null || !(target instanceof LivingEntity))
+							continue;
+						
+						CreatureHp.getCreatureHpByEntity((LivingEntity) target);
+						
+						AncientPlayerListener.damageignored = true;
+                        AncientEntityListener.ignoreNextHpEvent = true;
+                        ((LivingEntity) target).damage(damage, ca.getCaster());
+                        AncientPlayerListener.damageignored = false;
+                        AncientEntityListener.ignoreNextHpEvent = false;
                         
-                        Bukkit.getPluginManager().callEvent(event);
-                        
-                        if (event.isCancelled())
-                        	continue;
-                        
-                        AncientEntityListener.scheduledXpList.put(targetPlayer.getUniqueId(), ca.getCaster().getUniqueId());
-                        Ancient.plugin.scheduleThreadSafeTask(Ancient.plugin, new Runnable() {
-                            @Override
-                            public void run() {
-                                AncientEntityListener.scheduledXpList.remove(targetPlayer.getUniqueId());
-                            }
-                        }, Math.round(1000 / 50));
-                    }
-
-                    return true;
-                }
-            }
-        } catch (IndexOutOfBoundsException ignored) {
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return false;
-    }
+						AncientEntityListener.scheduledXpList.put(target.getUniqueId(), ca.getCaster().getUniqueId());
+						Ancient.plugin.scheduleThreadSafeTask(Ancient.plugin, new Runnable() {
+							@Override
+							public void run() {
+								AncientEntityListener.scheduledXpList.remove(target.getUniqueId());
+							}
+						}, Math.round(1000 / 50));
+					}
+					return true;
+				}
+			}
+		} catch (IndexOutOfBoundsException ignored) {}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return false;
+	}
 }
